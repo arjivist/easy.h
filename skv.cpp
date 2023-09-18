d795e397e16a227d3c0c38b22960069a92835c38
юлия <143997235+arjivist@users.noreply.github.com>
2023-09-18 21:15:05 +0300

    Add files via upload

---
+++ skv.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+int main() {
+    int num;
+    cin >> num;
+
+}
+
+int itc_skv(int num) {
+    if num > 0 {
+cout << num * num;
+}
+    else {
+        cout << "-1";
+    }
+}
---
+++ sqrt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+using namespace std;
+int main() {
+    int num;
+    cin >> num;
+    int otv = 1;
+
+}
+
+int itc_sqrt(int num) {
+    if (num > 0){
+    while (otv * otv < num) {
+otv = otv + 1;
+}}
+else {
+    cout << "-1";
+}
+}
