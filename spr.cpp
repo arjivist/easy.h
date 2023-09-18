69223868c1660f3a215379b4460f8db6fce71f1c
юлия <143997235+arjivist@users.noreply.github.com>
2023-09-18 21:23:51 +0300

    Add files via upload

---
+++ spr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a, b;
+
+}
+
+int itc_spr(int a, b) {
+    if (a * b > 0)  {
+cout << a * b;
+}
+    else {
+        cout << "-1";
+    }
+}
---
+++ str.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a, b, c;
+
+}
+
+int itc_str(int a, b, c) {
+    if ((a + b + c) / 2 > 0)  {
+cout << (a + b + c) / 2;
+}
+    else {
+        cout << "-1";
+    }
+}
