a62b0f02be487241e0756015a1268b44edf9965f
юлия <143997235+arjivist@users.noreply.github.com>
2023-09-18 20:15:49 +0300

    Add files via upload

---
+++ fabs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>;
+using namespace std;
+
+
+int main() {
+    int num;
+    cin >> num;
+
+}
+double itc_fabs(int num) {
+    if (num > 0) {
+cout << num; }
+else {
+if (num < 0) {
+    int minus_num = num * -1;
+    cout << minus_num;}
+    else {
+
+if (num == 0) {
+    cout << num;
+}}
+}
+return 0;
+}
+
+
